bb3e42f376b04f4d3abc4bcb482cee0fd7b140fe
Arjun <agir0003@student.monash.edu>
2023-05-03 21:57:02 +1000

    [Arjun G] Update new course modal

--- my-app/src/components/hero/Hero.jsx
+++ my-app/src/components/hero/Hero.jsx
@@ -1,16 +1,23 @@
-import React, {useState} from "react";
-import './hero.css';
+import React, { useState } from "react";
+import "./hero.css";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Hero = () => {
   const [modal, setModal] = useState(false);
 
   const toggleModal = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Proceed with your logic for submit action
+    window.location.href = "/specialisationpage";
+  };
 
   return (
     <div className="hero-container">
-
       <button 
       onClick={toggleModal}
       className="btn-modal">
@@ -20,83 +27,55 @@ const Hero = () => {
       <button className="btn-modal">
         View saved course plans
       </button>
-
-    {modal && (
-    <div className="modal">
-      <div className="overlay"></div>
-
-
-      <div className="modal-content">
-
-        <button className='close-modal'onClick={toggleModal}>X</button>
-
-        <h1 className="create-plan">Create your new course plan</h1>
-
-
-          <h1 className="modal-course-name">Name your plan</h1>
-          
-          <form>
-            <label>
-              <input type="text" className="plan-name" />
-            </label>
-          </form>
-
-          
-          <h1 className="modal-year-commencement">Year of commencement</h1>
-
-          <form>
-          <label>
-            <select className = "commencement-year">
-            <option value="2018-start">2018</option>
-            <option value="2019-start">2019</option>
-            <option value="2020-start">2020</option>
-            <option value="2021-start">2021</option>
-            <option value="2022-start">2022</option>
-            <option value="2023-start">2023</option>
-
-            </select>
-          </label>
-        </form>
-          
-          <h1 className="modal-study-load">Study load</h1>
-
-          <form>
-          <label>
-            <select className = "study-load">
-            <option value="full-time-study">Full-Time</option>
-            <option value="part-time-study">Part-Time</option>
-            </select>
-          </label>
-        </form>
-
-        <h1 className="year-12-units">Which Year 12 Unit(s) have you completed?</h1>
-
-          <form>
-          <label className="year-12-units-status">
-            <div> 
-              <input type="checkbox"/>
-              <label for="maths-34">VCE Specialist Mathemathics 3/4 </label>
-            </div>
-          
-          <div>
-            <input type="checkbox"/>
-            <label for="physics-34">VCE Physics 3/4 </label>
-            </div>
-
-          </label>
-        </form>
-
-        <a href="/specialisationpage">
-          <button className="create-plan-button">Submit</button>
-        </a>
-
-      </div>
-
+      {modal && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <button className='close-modal'onClick={toggleModal}>
+              <CloseIcon className="close-icon" fill="currentColor"/>
+            </button>
+            <h1 className="create-plan">Create your new course plan</h1>
+            <form onSubmit={handleSubmit}>
+            <h1 className="modal-course-name">Name your plan</h1>
+              <input type="text" className="plan-name" required />
+            <h1 className="modal-year-commencement">Year of commencement</h1>
+              <select className="commencement-year" required>
+                <option value="2018-start">2018</option>
+                <option value="2019-start">2019</option>
+                <option value="2020-start">2020</option>
+                <option value="2021-start">2021</option>
+                <option value="2022-start">2022</option>
+                <option value="2023-start">2023</option>
+              </select>
+            <h1 className="modal-study-load">Study load</h1>
+              <select className="study-load" required>
+                <option value="full-time-study">Full-Time</option>
+              </select>
+            <h1 className="year-12-units">Which Year 12 Unit(s) have you completed?</h1>
+              <div>
+                <input type="checkbox" required />
+                <label htmlFor="maths-34">VCE Specialist Mathemathics 3/4 </label>
+              </div>
+              <div>
+                <input type="checkbox" required />
+                <label htmlFor="physics-34">VCE Physics 3/4 </label>
+              </div>
+              <div>
+                <input type="checkbox" required />
+                <label htmlFor="none">None of the above </label>
+              </div>
+              <a href="/specialisationpage">
+                <button type="submit" className="create-plan-button">
+                  Submit
+                </button>
+              </a>
+              
+            </form>
+          </div>
+        </div>
+      )}
     </div>
-    )}
-    
-    </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
