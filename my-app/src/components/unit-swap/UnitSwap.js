1c5812e39d7382eefca88fdeeddf53b4f3fd63f0
Arjun-Girish <87962390+Arjun-Girish@users.noreply.github.com>
2023-05-05 13:19:04 +1000

    [Arjun G] created card component

--- my-app/src/components/unit-swap/UnitSwap.js
+++ my-app/src/components/unit-swap/UnitSwap.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./UnitSwap.css";
+import Card from "../unit-cards/Card";
 
 const App = () => {
 
@@ -9,20 +10,20 @@ const App = () => {
         id: 1,
         box_id: 1,
         semester_status: 3,
-        name: "ENG1013",
-        credit: "6",
+        unitCode: "ENG1013",
+        creditPoints: "6",
         unitTitle: "Engineering smart systems",
-        unitInfo: []
+        semesterOfferings: ["Semester 1", "Semester 2"]
       },
 
       {
         id: 2,
         box_id: 2,
         semester_status: 3,
-        name: "ENG1005",
-        credit: "6",
+        unitCode: "ENG1005",
+        creditPoints: "6",
         unitTitle: "Engineering Mathamatics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
 
@@ -30,60 +31,60 @@ const App = () => {
         id: 3,
         box_id: 3,
         semester_status: 3,
-        name: "ENG1014",
-        credit: "6",
+        unitCode: "ENG1014",
+        creditPoints: "6",
         unitTitle: "Engineering numerical analysis",
-        unitInfo: []
+        semesterOfferings: []
       },
   
       {
         id: 4,
         box_id: 4,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "First year elective",
-        unitInfo: []
+        semesterOfferings: []
       },
   
       {
         id: 5,
         box_id: 5,
         semester_status: 3,
-        name: "ENG1012",
-        credit: "6",
+        unitCode: "ENG1012",
+        creditPoints: "6",
         unitTitle: "Engineering design",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 6,
         box_id: 6,
         semester_status: 3,
-        name: "ENG1011",
-        credit: "6",
+        unitCode: "ENG1011",
+        creditPoints: "6",
         unitTitle: "Engineering methods",
-        unitInfo: []
+        semesterOfferings: []
       },
       
       {
         id: 7,
         box_id: 7,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 8,
         box_id: 8,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: [],
+        semesterOfferings: [],
       },
 
       
@@ -91,10 +92,10 @@ const App = () => {
         id: 9,
         box_id: 9,
         semester_status: 1,
-        name: "MEC2402",
-        credit: "6",
+        unitCode: "MEC2402",
+        creditPoints: "6",
         unitTitle: "Design methods",
-        unitInfo: []
+        semesterOfferings: []
       },
 
             
@@ -102,10 +103,10 @@ const App = () => {
         id: 10,
         box_id: 10,
         semester_status: 1,
-        name: "MEC2403",
-        credit: "6",
+        unitCode: "MEC2403",
+        creditPoints: "6",
         unitTitle: "Mechanics of materials",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       
@@ -113,40 +114,40 @@ const App = () => {
         id: 11,
         box_id: 11,
         semester_status: 3,
-        name: "ENG2005",
-        credit: "6",
+        unitCode: "ENG2005",
+        creditPoints: "6",
         unitTitle: "Advanced engineering mathematics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 12,
         box_id: 12,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 13,
         box_id: 13,
         semester_status: 2,
-        name: "MAE2402",
-        credit: "6",
+        unitCode: "MAE2402",
+        creditPoints: "6",
         unitTitle: "Thermodynamics and gas dynamics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 14,
         box_id: 14,
         semester_status: 2,
-        name: "MAE2404",
-        credit: "6",
+        unitCode: "MAE2404",
+        creditPoints: "6",
         unitTitle: "Aerodynamics 1",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       
@@ -154,80 +155,80 @@ const App = () => {
         id: 15,
         box_id: 15,
         semester_status: 2,
-        name: "MAE2505",
-        credit: "6",
+        unitCode: "MAE2505",
+        creditPoints: "6",
         unitTitle: "Aerospace dynamics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 16,
         box_id: 16,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 17,
         box_id: 17,
         semester_status: 1,
-        name: "MAE3401",
-        credit: "6",
+        unitCode: "MAE3401",
+        creditPoints: "6",
         unitTitle: "Aerodynamics 2",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 18,
         box_id: 18,
         semester_status: 1,
-        name: "MAE3404",
-        credit: "6",
+        unitCode: "MAE3404",
+        creditPoints: "6",
         unitTitle: "Flight vehicle dynamics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 19,
         box_id: 19,
         semester_status: 1,
-        name: "MEC3456",
-        credit: "6",
+        unitCode: "MEC3456",
+        creditPoints: "6",
         unitTitle: "Engineering computational analysis",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 20,
         box_id: 20,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 21,
         box_id: 21,
         semester_status: 2,
-        name: "MAE3405",
-        credit: "6",
+        unitCode: "MAE3405",
+        creditPoints: "6",
         unitTitle: "Aerospace propulsion",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 22,
         box_id: 22,
         semester_status: 2,
-        name: "MAE3408",
-        credit: "6",
+        unitCode: "MAE3408",
+        creditPoints: "6",
         unitTitle: "Aerospace control",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       
@@ -235,20 +236,20 @@ const App = () => {
         id: 23,
         box_id: 23,
         semester_status: 2,
-        name: "MAE3411",
-        credit: "6",
+        unitCode: "MAE3411",
+        creditPoints: "6",
         unitTitle: "Aerospace structural mechanics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 24,
         box_id: 24,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
 
@@ -256,30 +257,30 @@ const App = () => {
         id: 25,
         box_id: 25,
         semester_status: 3,
-        name: "ENG4701",
-        credit: "6",
+        unitCode: "ENG4701",
+        creditPoints: "6",
         unitTitle: "Final year project A",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 26,
         box_id: 26,
         semester_status: 1,
-        name: "MAE4416",
-        credit: "6",
+        unitCode: "MAE4416",
+        creditPoints: "6",
         unitTitle: "Orbital mechanics and spaceflight dynamics",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 27,
         box_id: 27,
         semester_status: 1,
-        name: "MEC4404",
-        credit: "6",
+        unitCode: "MEC4404",
+        creditPoints: "6",
         unitTitle: "Professional practice",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       
@@ -287,50 +288,50 @@ const App = () => {
         id: 28,
         box_id: 28,
         semester_status: 3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 29,
         box_id: 29,
         semester_status: 3,
-        name: "ENG4702",
-        credit: "6",
+        unitCode: "ENG4702",
+        creditPoints: "6",
         unitTitle: "Final year project B",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 30,
         box_id: 30,
         semester_status: 2,
-        name: "MAE4426",
-        credit: "6",
+        unitCode: "MAE4426",
+        creditPoints: "6",
         unitTitle: "Finite element analysis and composite structures",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 31,
         box_id: 31,
         semester_status: 2,
-        name: "MAE4410",
-        credit: "6",
+        unitCode: "MAE4410",
+        creditPoints: "6",
         unitTitle: "Flight vehicle design",
-        unitInfo: []
+        semesterOfferings: []
       },
 
       {
         id: 32,
         box_id: 32,
         semester_status:3,
-        name: "Elective",
-        credit: "6",
+        unitCode: "Elective",
+        creditPoints: "6",
         unitTitle: "Level 1,2,3 elective",
-        unitInfo: []
+        semesterOfferings: []
       },
      
   
@@ -343,7 +344,7 @@ const App = () => {
     let toIndex = boxes.findIndex((box) => box.id === toBox.id) ?? -1;
     
     let fromSem = boxes[fromIndex].semester_status;
-    let semName = boxes[toIndex].name;
+    let semunitCode = boxes[toIndex].unitCode;
     let toSem = boxes[toIndex].semester_status;
 
     console.log("to sem: " + toSem)
@@ -468,103 +469,69 @@ const App = () => {
 
   const makeBoxes = () => {
     return (
-      <div className="box-container">
+      <div classunitCode="box-container">
 
-        <div className="year-container">
+        <div classunitCode="year-container">
 
-          <div className="year-sem-container">
-            <h1 className="year-text">2023</h1>
+          <div classunitCode="year-sem-container">
+            <h1 classunitCode="year-text">2023</h1>
 
-            <div className="sem-container">
-              <p className="sem-text">Semester 1</p>
-              <p className="sem-text">Semester 2</p>
+            <div classunitCode="sem-container">
+              <p classunitCode="sem-text">Semester 1</p>
+              <p classunitCode="sem-text">Semester 2</p>
             </div>
 
           </div>
 
-          <div className="year-sem-container">
-            <h1 className="year-text">2024</h1>
+          <div classunitCode="year-sem-container">
+            <h1 classunitCode="year-text">2024</h1>
 
-            <div className="sem-container">
-              <p className="sem-text">Semester 1</p>
-              <p className="sem-text">Semester 2</p>
-            </div>
-            
-          </div>
-
-          <div className="year-sem-container">
-            <h1 className="year-text">2025</h1>
-
-            <div className="sem-container">
-              <p className="sem-text">Semester 1</p>
-              <p className="sem-text">Semester 2</p>
+            <div classunitCode="sem-container">
+              <p classunitCode="sem-text">Semester 1</p>
+              <p classunitCode="sem-text">Semester 2</p>
             </div>
             
           </div>
 
-          <div className="year-sem-container">
-            <h1 className="year-text">2026</h1>
+          <div classunitCode="year-sem-container">
+            <h1 classunitCode="year-text">2025</h1>
 
-            <div className="sem-container">
-              <p className="sem-text">Semester 1</p>
-              <p className="sem-text">Semester 2</p>
+            <div classunitCode="sem-container">
+              <p classunitCode="sem-text">Semester 1</p>
+              <p classunitCode="sem-text">Semester 2</p>
+            </div>
+            
+          </div>
+
+          <div classunitCode="year-sem-container">
+            <h1 classunitCode="year-text">2026</h1>
+
+            <div classunitCode="sem-container">
+              <p classunitCode="sem-text">Semester 1</p>
+              <p classunitCode="sem-text">Semester 2</p>
             </div>
             
           </div>
 
         </div>
 
-        <div className="unit-box-container">
-
-        {boxList.map((box, index) => (
-          <div
-            key={index}
-            className="box"
-            draggable={true}
-            onDragStart={handleDragStart({ id: box.id })}
-            onDragOver={handleDragOver({ id: box.id })}
-            onDrop={handleDrop({ id: box.id })}
-          >
-            <div className="content">
-              <div className="title-container">
-                <div className="unit-code">{box.name}</div>
-                <button className="unit-modal" onClick={()=>changeContent(box)}>ⓘ</button>
-
-              </div>
-
-              <div className="unit-title">{box.unitTitle}</div>
-
-            </div>
-          </div>
-
-        ))}
-        
-        {popupToggle&&<div className="popup-container" onClick={changeContent}>
-          <div className="popup-body" onClick={(e)=>e.stopPropagation()}> 
-            <div className="popup-header">
-              <button classaName="modal-close" onClick={changeContent}>x</button>              
-                <div className="modal-popup-content">
-                  {popupContent.map((box)=>{
-                    return(
-                      <div className="popup-card">
-                        <h1> {box.name} </h1>
-                        <p> {box.unitTitle} </p>
-                        <p> Credit Points: {box.credit}</p>
-                        <p> "This unit will help you to develop a systematic method of capturing design requirements, tools for ideation, estimation and decision-making. Primary and secondary manufacturing processes, assembly techniques. Engineering graphics for problem-solving, manufacturing communication and ideation. Report writing, teamwork in solving design problems involving the integration of mechanical elements in prototype conception, construction and testing."</p>
-                      </div>
-                    )
-                  })}
-              </div>
-            </div>
-          </div>
-          </div>}
-        </div>
+        <div>
+      {boxList.map((unit, index) => (
+        <Card
+          key={index}
+          unitTitle={unit.unitTitle}
+          unitCode={unit.unitCode}
+          creditPoints={unit.creditPoints}
+          semesterOfferings={unit.semesterOfferings}
+        />
+      ))}
+    </div>
       </div>
       
     );
   };
 
-  return <div className="boxesGroup">{makeBoxes()}</div>;
+  return <div classunitCode="boxesGroup">{makeBoxes()}</div>;
 };
 
 export default App;
